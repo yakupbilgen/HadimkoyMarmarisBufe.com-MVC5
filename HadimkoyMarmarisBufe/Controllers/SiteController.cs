34bb0025636047a89168f4992f1c3cd0b60e14ea
yakupbilgen92@outlook.com <yakupbilgen92@outlook.com>
2019-09-27 20:00:17 +0300

    www.hadimkoymarmarisbufe.com

---
+++ HadimkoyMarmarisBufe/Controllers/SiteController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HadimkoyMarmarisBufe.Controllers
+{
+	public class SiteController : Controller
+	{
+		// GET: Site
+		/*
+		 anasayfa
+		 ürünlerimiz
+		 menülerimiz
+		 hakkımızda
+		 neredeyiz?harita
+		 iletişim
+		 */
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Urunlerimiz()
+		{
+			return View();
+		}
+
+		public ActionResult Menulerimiz()
+		{
+			return View();
+		}
+
+		public ActionResult Hakkimizda()
+		{
+			return View();
+		}
+
+		public ActionResult Neredeyiz()
+		{
+			return View();
+		}
+
+		public ActionResult Iletisim()
+		{
+			return View();
+		}
+
+	}
+}
